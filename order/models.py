46a0370e37e0bf29f646125281a24f5b1796bf2c
Tarasidze <dtar84@ukr.net>
2023-07-20 13:25:03 +0300

    Create order-models and change pizzeria-settings

--- order/models.py
+++ order/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class Customer(AbstractUser):
+    date_of_birth = models.DateField()
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} {self.first_name} {self.last_name}"
+
+    # def get_absolute_url(self):
+    #     return reverse("order:customer-detail", kwargs={"pk": self.pk})
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    size = models.IntegerField()
+    prise = models.IntegerField()
+    quantity = models.IntegerField()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"Name: {self.name}, size: {self.size}, prise: {self.prise}"
+
+
+class Order(models.Model):
+    number_of_order = models.IntegerField(unique=True)
+    order_creation_date = models.DateTimeField(auto_now_add=True)
+    pizzas = models.ManyToManyField(Pizza, related_name="order")
+    customer = models.ManyToManyField(Customer, related_name="order")
+
+    class Meta:
+        ordering = ["-order_creation_date"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["customer", "number_of_order"],
+                name="unique order"
+            )
+        ]
--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'order',
 ]
 
 MIDDLEWARE = [
@@ -87,6 +89,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = "order.Customer"
 
 
 # Internationalization
