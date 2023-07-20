caa48a7acb54d578d20dca4de4bfc5d47a1f519d
Tarasidze <dtar84@ukr.net>
2023-07-20 14:29:03 +0300

    Create ingradients model and add relation to pizza

--- order/models.py
+++ order/models.py
@@ -16,11 +16,19 @@ class Customer(AbstractUser):
     #     return reverse("order:customer-detail", kwargs={"pk": self.pk})
 
 
+class Ingredients(models.Model):
+    ingredient = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.ingredient
+
+
 class Pizza(models.Model):
     name = models.CharField(max_length=255, unique=True)
     size = models.IntegerField()
     prise = models.IntegerField()
     quantity = models.IntegerField()
+    ingredients = models.ManyToManyField(Ingredients, related_name="pizzas")
 
     class Meta:
         ordering = ["name"]
@@ -32,8 +40,8 @@ class Pizza(models.Model):
 class Order(models.Model):
     number_of_order = models.IntegerField(unique=True)
     order_creation_date = models.DateTimeField(auto_now_add=True)
-    pizzas = models.ManyToManyField(Pizza, related_name="order")
-    customer = models.ManyToManyField(Customer, related_name="order")
+    pizzas = models.ManyToManyField(Pizza, related_name="orders")
+    customer = models.ManyToManyField(Customer, related_name="orders")
 
     class Meta:
         ordering = ["-order_creation_date"]
