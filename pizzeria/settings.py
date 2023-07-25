48ff697afffd426c6892f0714c4b6ad72a0e86d5
Tarasidze <dtar84@ukr.net>
2023-07-25 12:09:05 +0300

    rewrite settings.py and requirements.txt

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -1,22 +1,20 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import decouple
+from dotenv import load_dotenv
+import dj_database_url
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
+SECRET_KEY = decouple.config("SECRET_KEY", default="VerySecretKey", cast=str)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-qe=j6a8aswh+9ala7z@y7t=!ikvd!6&5qf@if8ub5=qvls&k^z")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = decouple.config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -35,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -66,9 +65,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 WSGI_APPLICATION = 'pizzeria.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -76,9 +72,14 @@ DATABASES = {
     }
 }
 
+# dj-database-url
+db_from_env = dj_database_url.config(
+    default=decouple.config("DATABASE_URL"),
+    conn_max_age=500
+)
 
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+DATABASES["default"].update(db_from_env)
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -97,9 +98,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "order.Customer"
 
+LOGIN_REDIRECT_URL = "/"
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -108,10 +108,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
